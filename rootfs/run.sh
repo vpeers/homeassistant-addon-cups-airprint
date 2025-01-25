a975d25867ae734b6df000a6f0aedcfe2c50ae6b
vpeers <jakub@koy.com.pl>
2025-01-25 01:17:59 +0100

    Update run.sh

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -7,7 +7,7 @@ until [ -e /var/run/avahi-daemon/socket ]; do
 done
 
 bashio::log.info "Preparing directories"
-mount --bind /data/cups/cupsd.conf /etc/cups/cupsd.conf
+mount --bind /data/cups/cupsd.conf /etc/cups/cupsd.conf || echo || dmesg
 
 bashio::log.info "Starting CUPS server as CMD from S6"
 
